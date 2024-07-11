4a714dd3863f2f2e332cc9cf5d5083b7f48d0f7a
grzla <greg.j.lundberg@gmail.com>
2024-07-10 20:37:56 -0400

    npm start. implement index page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+import * as React from 'react';
 import logo from './logo.svg';
+import { styled } from '@mui/material/styles';
+import Test2 from './components/SongList.tsx'
+import BasicList from './components/BasicList.tsx'
+import {Typography} from '@mui/material'
+import Grid from '@mui/material/Grid';
+import { Box, Container, Stack } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import CommandBar from './components/CommandBar.tsx'
+import Home from './Home.tsx'
+import SearchBar from './components/SearchBar.tsx'
 import './App.css';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Box>
+          <Grid container spacing={0}
+          sx={{
+            mt: '2px'
+          }}
+          >
+              <Item sx={{ flexGrow: '6'}}>
+                  <CommandBar/>
+                  <BasicList/>
+              </Item>
+              <Item sx={{ flexGrow: '3'}}>
+                <SearchBar/>
+              </Item>
+          </Grid>
+        </Box>
     </div>
   );
 }
---
+++ src/util.ts
@@ -0,0 +1,44 @@
+import {Item} from "./typings"
+
+export const testSongs: Song[] = [
+    {
+      id: "1",
+      artist: "Dexy's Midnight Runners",
+      title: "Come On Eileen",
+      year: 2019,
+      rank: 5,
+      releaseYear: null,
+    },
+    {
+      id: "2",
+      artist: "Blur",
+      title: "Song 2",
+      year: 2019,
+      rank: 3,
+      releaseYear: null,
+    },
+    {
+      id: "3",
+      artist: "Tina Turner",
+      title: "We Don't Need Another Hero",
+      year: 2019,
+      rank: 2,
+      releaseYear: null,
+    },
+    {
+      id: "4",
+      artist: "Song 4",
+      title: "Artist 4",
+      year: 2019,
+      rank: 1,
+      releaseYear: null,
+    },
+    {
+      id: "5",
+      artist: "Song 5",
+      title: "Artist 5",
+      year: 2019,
+      rank: 10,
+      releaseYear: null,
+    },
+  ];
